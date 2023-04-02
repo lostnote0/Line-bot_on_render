da5f494cf34ad88098e9426215d2203a39d0d338
lostnote0 <128904170+lostnote0@users.noreply.github.com>
2023-04-02 19:46:42 +0800

    Update app.py

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -72,6 +72,23 @@ def callback():
 
     return 'OK'
 
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+        line_bot_api = LineBotApi('你的 Channel access token')
+        handler = WebhookHandler('你的 LINE Channel secret')
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
+    except:
+        print('error')
+    return 'OK'
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
